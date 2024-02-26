24365315dd7f899f1eb9399945bbefcbfc95ed1b
army711 <armandocavallo@gmail.com>
2024-02-26 23:21:52 +0100

    modifica nome CSAR

--- script/CSAR.lua
+++ script/CSAR.lua
@@ -1,35 +1,46 @@
 -- Crea un nuovo oggetto CSAR per il lato BLUE con il nome "CSARPilot" e il beacon "SOS-Beacon"
 local MedorentCSAR = CSAR:New(coalition.side.BLUE, "CSARPilot", "SOS-Beacon")
 
--- Imposta l'equipaggio come immortale e invisibile
-MedorentCSAR.immortalcrew = true
-MedorentCSAR.invisiblecrew = true
-
--- Abilita il Bronco come mezzo di recupero
-MedorentCSAR.allowbronco = true
-
--- Imposta il nome del menu principale
-MedorentCSAR.topmenuname = "Medorent Combat Search & Rescue"
-
--- Abilita l'uso del prefisso "CSAR"
-MedorentCSAR.useprefix = true
-MedorentCSAR.csarPrefix = {"CSAR","Helicargo"}
+-- Imposta le opzioni del CSAR
+MedorentCSAR.immortalcrew = true -- Equipaggio immortale
+MedorentCSAR.invisiblecrew = true -- Equipaggio invisibile
+MedorentCSAR.allowbronco = true -- Abilita il Bronco come mezzo di recupero
+MedorentCSAR.topmenuname = "Medorent Combat Search & Rescue" -- Imposta il nome del menu principale
+MedorentCSAR.useprefix = true -- Abilita il prefisso per i CSAR
+MedorentCSAR.csarPrefix = {"CSAR","Helicargo"} -- Imposta i prefissi per i CSAR
 
 -- Inizializza variabile activeCsarMissions
 
 local activeCsarMissions = 0
 
--- Funzione chiamata quando un pilota viene abbattuto
+-- Modifica messaggi Standard
+
 function MedorentCSAR:OnAfterPilotDown(From, Event, To, SpawnedGroup, Frequency, Leadername, CoordinatesText)
     local activeCsarMissions = self:_CountActiveDownedPilots()
     MESSAGE:New(string.format("Il pilota %s è abbattuto! La frequenza per il CSAR è %s KHz, le coordinate sono %s.", Leadername, Frequency, CoordinatesText), 15):ToAll()
 end
-
 -- Funzione chiamata quando un pilota viene recuperato
 function MedorentCSAR:OnAfterRescued(From, Event, To, HeliUnit, HeliName, PilotsSaved)
     local activeCsarMissions = self:_CountActiveDownedPilots()
     MESSAGE:New("Missione CSAR terminata con successo. Missioni attive:" .. activeCsarMissions, 30):ToAll()
 end
+-- Funzione chiamata quando Ci si avvicina ad un pilota Abbattuto
+-- function MedorentCSAR:OnAfterApproach(from, event, to, heliname, groupname)
+--     local activeCsarMissions = self:_CountActiveDownedPilots()
+--     MESSAGE:New("Sento il rumore del motore! Richiedi un fumogeno o un flare se necessario.", 30):ToAll()
+-- end
+-- -- Funzione chiamata quando un pilota viene recuperato
+-- function MedorentCSAR:OnAfterBoarded(from, event, to, heliname, groupname, description)
+--     local activeCsarMissions = self:_CountActiveDownedPilots()
+--     MESSAGE:New("Pilota a bordo. Torna alla base", 30):ToAll()
+-- end
+-- Funzione chiamata quando un pilota viene riportato in base
+-- function MedorentCSAR:OnAfterRescued(from, event, to, heliunit, heliname, pilotssaved)
+--     local activeCsarMissions = self:_CountActiveDownedPilots()
+--     MESSAGE:New("Pilota recuperato. Ben Fatto! Missioni attive: " .. activeCsarMissions, 30):ToAll()
+-- end
+
+-- Fine Modifica messaggi Standard
 
 -- Avvia il CSAR
 MedorentCSAR:Start()
@@ -44,11 +55,11 @@ local function startCsarMission()
     -- Controlla se ci sono meno di 5 missioni CSAR attive
     if activeCsarMissions <= 5 then
         -- Avvia una nuova missione CSAR nella zona CSAR
-        MedorentCSAR:SpawnCSARAtZone(csarZone, coalition.side.BLUE, "PilotaAbbattuto", true)
+        MedorentCSAR:SpawnCSARAtZone(csarZone, coalition.side.BLUE, "DSMC_NoUp_", true, false, false, "CSAR-Random")
         MESSAGE:New("Missioni CSAR attive: " .. activeCsarMissions, 30):ToAll()
     end
 end
 
 -- Crea un scheduler per controllare le missioni CSAR attive ogni 60 secondi
-local checkActiveMissionsScheduler = SCHEDULER:New(nil, startCsarMission, {}, 0, 1800)
+local checkActiveMissionsScheduler = SCHEDULER:New(nil, startCsarMission, {}, 0, 10)
 checkActiveMissionsScheduler:Start()
