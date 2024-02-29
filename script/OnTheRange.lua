70310941247ff189917fc572b77fa1f592a73705
army711 <armandocavallo@gmail.com>
2024-02-29 13:25:20 +0100

    disattivati setautosave su OnTheRange

--- script/OnTheRange.lua
+++ script/OnTheRange.lua
@@ -10,8 +10,8 @@ Rayakrange:AddStrafePit(strafepit_gun, nil, nil, nil, true, 5, nil)
 Rayakrange:AddBombingTargets(bombpit, 10)
 Rayakrange:SetDefaultPlayerSmokeBomb(false)
 Rayakrange:GetFoullineDistance("StrafePit-1", "Foulline-1")
-Rayakrange:SetAutoSaveOn()
-Rayakrange:SetTargetSheet("C:\\temp\\MedorentCache","RayakRange")
+--Rayakrange:SetAutoSaveOn()
+--Rayakrange:SetTargetSheet("C:\\temp\\MedorentCache","RayakRange")
 -- Imposta cartella di salvataggio storico Range
 
 Rayakrange:SetSaveFolder("C:\\temp\\MedorentCache")
