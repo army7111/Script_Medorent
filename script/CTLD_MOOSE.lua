8b2d0b61c7524e03b93b5b54b242dc19f51b3311
army711 <armandocavallo@gmail.com>
2024-02-29 13:18:32 +0100

    nuovoBranchFixCTLD_Range

--- script/CTLD_MOOSE.lua
+++ script/CTLD_MOOSE.lua
@@ -1,31 +1,30 @@
 -- Crea e attiva CTLD nella missione
-ctld_medorent = CTLD:New(coalition.side.BLUE, {"Helicargo","CSAR"},"CTLD Medorent")
-
+CTLD_medorent = CTLD:New(coalition.side.BLUE, {"Helicargo","CSAR"},"CTLD Medorent")
+CTLD_medorent:__Start(5)
 -- Inizio Parametri e opzioni di configurazione
-ctld_medorent.dropcratesanywhere = true -- Option to allow crates to be dropped anywhere.
-ctld_medorent.enableHercules = true
-ctld_medorent.forcehoverload = false
-ctld_medorent.CrateDistance = 100
+CTLD_medorent.dropcratesanywhere = true -- Option to allow crates to be dropped anywhere.
+CTLD_medorent.enableHercules = true
+CTLD_medorent.forcehoverload = false
+CTLD_medorent.CrateDistance = 100
 -- Fine Parametri e opzioni di configurazione
 
 -- Tipi di unità utilizzabili
 ---- Truppe
-ctld_medorent:AddTroopsCargo("Ingegneri",{"Engineer"}, CTLD_CARGO.Enum.ENGINEERS,4,80)
--- ctld_medorent:EngineerSearch = 2000
-ctld_medorent:AddTroopsCargo("Anti-Tank Squadra Piccola",{"ATS"},CTLD_CARGO.Enum.TROOPS,3,80)
-ctld_medorent:AddTroopsCargo("Anti-Air Squadra Piccola",{"AA_Inf"},CTLD_CARGO.Enum.TROOPS,3,80)
+CTLD_medorent:AddTroopsCargo("Ingegneri",{"Engineer"}, CTLD_CARGO.Enum.ENGINEERS,4,80)
+-- CTLD_medorent:EngineerSearch = 2000
+CTLD_medorent:AddTroopsCargo("Anti-Tank Squadra Piccola",{"ATS"},CTLD_CARGO.Enum.TROOPS,3,80)
+CTLD_medorent:AddTroopsCargo("Anti-Air Squadra Piccola",{"AA_Inf"},CTLD_CARGO.Enum.TROOPS,3,80)
 ---- Veicoli in Box
-ctld_medorent:AddCratesCargo("Humvee Scout",{"HeliRecon-"},CTLD_CARGO.Enum.VEHICLE,1,2000)
-ctld_medorent:AddCratesCargo("SHORAD",{"SHORAD_Temp"},CTLD_CARGO.Enum.VEHICLE,2,2775)
---ctld_medorent:AddCratesCargo("Forward Ops Base",{"FOB"},CTLD_CARGO.Enum.FOB,8,2775)
-ctld_medorent:AddCratesCargo("Postazione NASAMS Completa",{"NASAMSTemplate"},CTLD_CARGO.Enum.VEHICLE,7,2775)
-ctld_medorent:AddCratesCargo("Paladin Singolo Artiglieria",{"PaladinM109Template"},CTLD_CARGO.Enum.VEHICLE,3,2775)
+CTLD_medorent:AddCratesCargo("Humvee Scout",{"HeliRecon-"},CTLD_CARGO.Enum.VEHICLE,1,2000)
+CTLD_medorent:AddCratesCargo("SHORAD",{"SHORAD_Temp"},CTLD_CARGO.Enum.VEHICLE,2,2775)
+--CTLD_medorent:AddCratesCargo("Forward Ops Base",{"FOB"},CTLD_CARGO.Enum.FOB,8,2775)
+CTLD_medorent:AddCratesCargo("Postazione NASAMS Completa",{"NASAMSTemplate"},CTLD_CARGO.Enum.VEHICLE,7,2775)
+CTLD_medorent:AddCratesCargo("Paladin Singolo Artiglieria",{"PaladinM109Template"},CTLD_CARGO.Enum.VEHICLE,3,2775)
 -- Fine Tipi di unità utilizzabili
 
 -- Dichiarazione delle zone di carico
-ctld_medorent:AddCTLDZone("ZonaCarico",CTLD.CargoZoneType.LOAD,SMOKECOLOR.Blue,true,true)
-ctld_medorent:AddCTLDZone("ZonaCarico-1",CTLD.CargoZoneType.LOAD,SMOKECOLOR.Blue,true,true)
-ctld_medorent:AddCTLDZone("ZonaCarico-2",CTLD.CargoZoneType.LOAD,SMOKECOLOR.Blue,true,true)
-ctld_medorent:AddCTLDZone("ZonaCarico-3",CTLD.CargoZoneType.LOAD,SMOKECOLOR.Blue,true,true)
+CTLD_medorent:AddCTLDZone("ZonaCarico",CTLD.CargoZoneType.LOAD,SMOKECOLOR.Blue,true,true)
+CTLD_medorent:AddCTLDZone("ZonaCarico-1",CTLD.CargoZoneType.LOAD,SMOKECOLOR.Blue,true,true)
+CTLD_medorent:AddCTLDZone("ZonaCarico-2",CTLD.CargoZoneType.LOAD,SMOKECOLOR.Blue,true,true)
+CTLD_medorent:AddCTLDZone("ZonaCarico-3",CTLD.CargoZoneType.LOAD,SMOKECOLOR.Blue,true,true)
 -- Start CTLD
-ctld_medorent:Start()
--- script/OnTheRange.lua
+++ script/OnTheRange.lua
@@ -10,6 +10,11 @@ Rayakrange:AddStrafePit(strafepit_gun, nil, nil, nil, true, 5, nil)
 Rayakrange:AddBombingTargets(bombpit, 10)
 Rayakrange:SetDefaultPlayerSmokeBomb(false)
 Rayakrange:GetFoullineDistance("StrafePit-1", "Foulline-1")
+Rayakrange:SetAutoSaveOn(true)
+Rayakrange:SetTargetSheet("C:\\temp\\MedorentCache","RayakRange")
+-- Imposta cartella di salvataggio storico Range
+
+Rayakrange:SetSaveFolder("C:\\temp\\MedorentCache")
 
 -- Start Range
 Rayakrange:Start()
