8eb50d7ba1d3c1eafed0a0d999c17d16dae50fb1
army711 <armandocavallo@gmail.com>
2024-03-07 20:20:30 +0100

    Aggiornato script CipratA2ADispatcher.lua

--- script/CipratA2ADispatcher.lua
+++ script/CipratA2ADispatcher.lua
@@ -17,10 +17,10 @@ BlueHQCipratt = COMMANDCENTER:New(BlueCCCipratPosi, "Cipro Attack Command Center
 CiprattMission = MISSION:New(BlueHQCipratt, "Cipro Attack Missions", "Primary", "Missioni A2A Cipro", coalition.side.BLUE)
 
 AwacsTrigger = ZONE:New("AWACSZone")
-local AwacsCipratt = AUFTRAG:NewAWACS((AwacsTrigger:GetCoordinate()), 30000, 400, 359, 30)
+local AwacsCipratt = AUFTRAG:NewAWACS(AwacsTrigger:GetCoordinate(), 30000, 400, 359, 30)
 
 AwacsCipratt:SetTime("07:00", "20:00")
-AwacsCipratt:SetTACAN(29, "AWA")
+-- AwacsCipratt:SetTACAN(29, "AWA")
 AwacsCipratt:SetRadio(247)
 AwacsCipratt:SetImmortal(true)
 
