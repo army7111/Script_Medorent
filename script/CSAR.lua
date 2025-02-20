85337a0bf60aad7b9822ed715e3498b776a66d8e
Army711 <army711live@gmail.com>
2025-02-20 18:15:04 +0100

    Fix CSAR object initialization by removing unnecessary local declaration

--- script/CSAR.lua
+++ script/CSAR.lua
@@ -1,5 +1,5 @@
 -- Crea un nuovo oggetto CSAR per il lato BLUE con il nome "CSARPilot" e il beacon "SOS-Beacon"
-local MedorentCSAR = CSAR:New(coalition.side.BLUE, "CSARPilot", "SOS-Beacon")
+MedorentCSAR = CSAR:New(coalition.side.BLUE, "CSARPilot", "SOS-Beacon")
 MedorentCSAR.enableLoadSave = true -- Abilita il salvataggio e il caricamento delle missioni CSAR
 MedorentCSAR.saveinterval = 600 -- Imposta l'intervallo di salvataggio delle missioni CSAR
 MedorentCSAR.filename = "CSARMedorent.csv" -- Imposta il nome del file per il salvataggio delle missioni CSAR
